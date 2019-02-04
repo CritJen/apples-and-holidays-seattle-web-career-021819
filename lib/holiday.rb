3d464b2a9df07bd35d278fca1c61768778a216fb
Jennifer Clark <jen.boyd.clark@gmail.com>
2019-02-04 17:18:43 +0000

    Automatically backed up by Learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -48,9 +48,9 @@ end
 def all_holidays_with_bbq(holiday_hash)
   holiday_hash.each do |season, holiday|
     holiday.select!{|name, supplies| supplies.include?("BBQ")}
-    end
     if season.empty?
       return holiday
+    end
     end
   end
 
